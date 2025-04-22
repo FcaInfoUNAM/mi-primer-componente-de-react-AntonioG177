4004395c853d7f3532ed9a514508bbb096607467
Antonio Guadiana <119093084+AntonioG177@users.noreply.github.com>
2025-04-22 01:27:10 +0000

    Mi primer componente de React

--- my-app/src/Grid.jsx
+++ my-app/src/Grid.jsx
@@ -1,3 +1,5 @@
-export function Grid(){
-    /* Escrobe aquí tu código */
+export function Grid({items}){
+    return <nav className="grid">
+          {items.map(e => <a className="nav-item" href={e.ref}>{e.title}</a>)}
+      </nav>
 }
